7af6f89262f4d31a64fb50cd67ea61b20d9663b1
Birger E <bibbevidio@gmail.com>
2024-08-12 12:38:02 +0200

    Bolt rotation lock up

--- Assets/HurricaneVR/Framework/Scripts/Weapons/Guns/HVRBoltAction.cs
+++ Assets/HurricaneVR/Framework/Scripts/Weapons/Guns/HVRBoltAction.cs
@@ -14,5 +14,15 @@ namespace HurricaneVR.Framework.Weapons.Guns
             EjectCasingAfterFiring = false;
             BoltPushedBackAfterEmpty = false;
         }
+        
+        protected override bool CanFire()
+        {
+            if (CockingHandle is not HVRRotatingBoltHandle boltHandle) return true;
+            //Check if rotating bolt handle is closed before we can fire
+            if (boltHandle.rotationValue01 > .1f) return false;
+
+            return base.CanFire();
+        }
+
     }
 }
