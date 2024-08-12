906a3c81a91ff80f3172ff59c00f2f9590ea0491
Birger E <bibbevidio@gmail.com>
2024-08-12 12:17:34 +0200

    namespace

--- Assets/HVRRotatingBoltHandle.cs
+++ Assets/HVRRotatingBoltHandle.cs
@@ -6,50 +6,67 @@ using HurricaneVR.Framework.Weapons.Guns;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class HVRRotatingBoltHandle : HVRCockingHandle
+namespace HurricaneVR.Framework.Weapons.Guns
 {
-    public Vector3 handleRotationEulerAngles;
-    public Vector3 boltRotationEulerAngles;
-    public float RotateDifficulty = .05f;
-    public Transform rotateUp;
-
-    private float _lockedValue01;
-    private bool IsBoltLocked => _lockedValue01 < .1f;
-    private float _startingPullDifficulty;
-    private Quaternion _startingLocalRotation;
-    private Quaternion _startingLocalBoltRotation;
-
-    // Start is called before the first frame update
-    protected override void Awake()
+    public class HVRRotatingBoltHandle : HVRCockingHandle
     {
-        base.Awake();
-        
-        _startingPullDifficulty = Difficulty;
-        _startingLocalRotation = transform.localRotation;
-        _startingLocalBoltRotation = Bolt.transform.localRotation;
-    }
+        public Vector3 handleRotationEulerAngles;
+        public Vector3 boltRotationEulerAngles;
+        public float RotateDifficulty = .05f;
+        public Transform rotateUp;
 
-    protected override void Update()
-    {
-        base.Update();
-        if (Grabbable.HandGrabbers.Count > 0)
+        private float _lockedValue01;
+        private bool IsBoltLocked => _lockedValue01 < .1f;
+        private float _startingPullDifficulty;
+        private Quaternion _startingLocalRotation;
+        private Quaternion _startingLocalBoltRotation;
+
+        // Start is called before the first frame update
+        protected override void Awake()
         {
-            var deltaHandMovement = (Grabbable.HandGrabbers[0].TrackedController.position - _grabbedPositionTracker.position);
-            var localUpDirection = rotateUp.up.normalized * 10;
-            var amount = Vector3.Dot(deltaHandMovement, localUpDirection) * RotateDifficulty;
-            _lockedValue01 += amount;
-            _lockedValue01 = Mathf.Clamp01(_lockedValue01);
+            base.Awake();
 
-            if (Vector3.Distance(transform.position, ForwardPositionWorld) > .1f)
-                _lockedValue01 = 1;
-            
-            transform.localRotation = Quaternion.Lerp(_startingLocalRotation, Quaternion.Euler(handleRotationEulerAngles), _lockedValue01);
-            Bolt.transform.localRotation = Quaternion.Lerp(_startingLocalBoltRotation, Quaternion.Euler(boltRotationEulerAngles), _lockedValue01);
+            _startingPullDifficulty = Difficulty;
+            _startingLocalRotation = transform.localRotation;
+            _startingLocalBoltRotation = Bolt.transform.localRotation;
         }
 
-        Difficulty = IsBoltLocked ? 0 : _startingPullDifficulty;
-    }
+        protected override void Update()
+        {
+            base.Update();
+            if (Grabbable.HandGrabbers.Count > 0)
+            {
+                var deltaHandMovement = (Grabbable.HandGrabbers[0].TrackedController.position -
+                                         _grabbedPositionTracker.position);
+                var localUpDirection = rotateUp.up.normalized * 10;
+                var amount = Vector3.Dot(deltaHandMovement, localUpDirection) * RotateDifficulty;
+                _lockedValue01 += amount;
+                _lockedValue01 = Mathf.Clamp01(_lockedValue01);
+
+                if (Vector3.Distance(transform.position, ForwardPositionWorld) > .1f)
+                    _lockedValue01 = 1;
+
+                transform.localRotation = Quaternion.Lerp(_startingLocalRotation,
+                    Quaternion.Euler(handleRotationEulerAngles), _lockedValue01);
+                Bolt.transform.localRotation = Quaternion.Lerp(_startingLocalBoltRotation,
+                    Quaternion.Euler(boltRotationEulerAngles), _lockedValue01);
+            }
+
+            Difficulty = IsBoltLocked ? 0 : _startingPullDifficulty;
+        }
+/*
+    protected override void CheckChamberDistance(float distance)
     {
+        print($"rotation val {_lockedValue01}");
+        //Check if bolt has been rotated closed
+        if (_lockedValue01 > .1f)
+        {
+            _chamberDistanceReached = false;
+            return;
+        }
         
+        print($"checking regular");
+        base.CheckChamberDistance(distance);
     }*/
+    }
 }
